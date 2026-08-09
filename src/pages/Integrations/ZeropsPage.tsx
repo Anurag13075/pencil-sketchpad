c7c5686fe9ca322de51206d8659c9fe2feef98da
Anurag Sharma <yourboyanurag777@gmail.com>
2026-08-09 10:18:53 +0530

    Update ZeropsPage.tsx

--- src/pages/Integrations/ZeropsPage.tsx
+++ src/pages/Integrations/ZeropsPage.tsx
@@ -4,7 +4,7 @@ import ConfigGenerator from "@/integrations/zerops/ConfigGenerator";
 export default function ZeropsPage() {
   return (
     <div className="container mx-auto p-6 space-y-6">
-      <QuickStartPanel />
+      <QuickStartPanel /> 
       <ConfigGenerator />
     </div>
   );
